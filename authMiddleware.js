d47d8eb7290b89ab160cd892bcf7aab8be206553
Lucas Neves <lzmn70@gmail.com>
2025-06-01 06:52:10 -0300

    Desenvolvimento dos métodos para JWT e adição do Bcrypt

---
+++ authMiddleware.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+
+let secret;
+
+function obterSecret() {
+
+    if (secret != undefined) return secret;
+
+    const secretPath = path.join(__dirname, "jwtsecret.json");
+    const dados = fs.readFileSync(secretPath);
+
+    secret = JSON.parse(dados).Secret;
+
+    return secret;
+}
+
+function verificarJWT(req, res, next) {
+
+    const header = req.headers.authorization;
+
+    if (!header) 
+        return res.status(401).send({ message: 'Token não encontrado' });
+
+    const token = header.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, obterSecret());
+
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+
+        if (error.name == "TokenExpiredError")
+            return res.status(401).send({ message: 'Token inválido ou expirado' });
+    }
+}
+
+function gerarJWT(dados) {
+    const tokenGerado = jwt.sign(dados, obterSecret(), { expiresIn: '3d' });
+    return tokenGerado;
+}
+
+module.exports = {
+    verificarJWT,
+    gerarJWT
+}
